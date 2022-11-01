bda5892381faf430c8b11969f67d609da0ef2432
Muh Suryadi Triputra Rahman <msuryaditriputra@gmail.com>
2022-11-01 21:47:19 +0800

    change color menu when active

--- assets/js/main.js
+++ assets/js/main.js
@@ -37,6 +37,28 @@ const scrollHeader = () => {
 window.addEventListener("scroll", scrollHeader);
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll("section[id]");
+
+function scrollActive() {
+    const scrollY = window.scrollY;
+
+    sections.forEach((current) => {
+        const sectionHeight = current.offsetHeight;
+        const sectionTop = current.offsetTop - 58;
+        const sectionId = current.getAttribute("id");
+        const sectionClass = document.querySelector(
+            `.nav__menu a[href*=${sectionId}]`
+        );
+
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            sectionClass.classList.add("active-link");
+        } else {
+            sectionClass.classList.remove("active-link");
+        }
+    });
+}
+
+window.addEventListener("scroll", scrollActive);
 
 /*=============== SHOW SCROLL UP ===============*/
 
